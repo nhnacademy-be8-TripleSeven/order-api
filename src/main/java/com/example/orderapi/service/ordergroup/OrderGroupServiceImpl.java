df2d137e88b5e98c6bb21a58eaa7c1ded70cefeb
wjpark4430 <pwj4na2@gmail.com>
2024-12-14 10:29:25 +0900

    a

--- src/main/java/com/example/orderapi/service/ordergroup/OrderGroupServiceImpl.java
+++ src/main/java/com/example/orderapi/service/ordergroup/OrderGroupServiceImpl.java
@@ -33,8 +33,9 @@ public class OrderGroupServiceImpl implements OrderGroupService {
 
     @Override
     public Page<OrderGroupResponse> getOrderGroupsByUserId(Long userId, Pageable pageable) {
-        Page<OrderGroup> orderGroups = orderGroupRepository.findAllByUserId(userId, pageable);
-        return orderGroups.map(OrderGroupResponse::fromEntity);
+//        Page<OrderGroup> orderGroups = orderGroupRepository.findAllByUserId(userId, pageable);
+//        return orderGroups.map(OrderGroupResponse::fromEntity);
+        return null;
     }
 
     @Override
