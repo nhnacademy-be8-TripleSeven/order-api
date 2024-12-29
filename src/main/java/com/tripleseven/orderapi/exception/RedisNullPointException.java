7b07b76805e43dfc0fe1e38d89ae7743f33f04e4
wjpark4430 <pwj4na2@gmail.com>
2024-12-29 15:36:45 +0900

    feat: add exception

---
+++ src/main/java/com/tripleseven/orderapi/exception/RedisNullPointException.java
@@ -0,0 +1,7 @@
+package com.tripleseven.orderapi.exception;
+
+public class RedisNullPointException extends RuntimeException {
+    public RedisNullPointException(String message) {
+        super(message);
+    }
+}
