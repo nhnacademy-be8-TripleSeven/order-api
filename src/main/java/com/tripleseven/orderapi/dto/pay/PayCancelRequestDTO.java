37d37414db2291f31e4f45e1f9a36db31729fbf0
wjpark4430 <pwj4na2@gmail.com>
2025-01-17 14:39:23 +0900

    feat: 주문 저장 실패 시 로직 추가

--- src/main/java/com/tripleseven/orderapi/dto/pay/PayCancelRequestDTO.java
+++ src/main/java/com/tripleseven/orderapi/dto/pay/PayCancelRequestDTO.java
@@ -1,10 +1,16 @@
 package com.tripleseven.orderapi.dto.pay;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class PayCancelRequestDTO {
     @JsonProperty
     private String cancelReason;
     @JsonProperty
     private Long cancelAmount;
+
+    public PayCancelRequestDTO(String cancelReason) {
+        this.cancelReason = cancelReason;
+    }
 }
