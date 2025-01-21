a848d3a3ff43afacbe3d151db4d33063173cd689
jmg5642 <1>
2025-01-21 14:28:51 +0900

    chore: PointServiceImpl 코드 수정

--- src/main/java/com/tripleseven/orderapi/business/feign/MemberService.java
+++ src/main/java/com/tripleseven/orderapi/business/feign/MemberService.java
@@ -4,4 +4,6 @@ import java.util.List;
 
 public interface MemberService {
     void clearCart(Long userId, String guestId, List<Long> bookIds);
+
+    Long getGradePoint(Long userId, long totalAmount);
 }
